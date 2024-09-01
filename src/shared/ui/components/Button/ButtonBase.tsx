9cee8a1a277eb094d807bef37303c30d61b4f5a6
ewanivannikov <ewanivannikov@gmail.com>
2024-09-01 10:41:06 +0300

    bug: fix button base

--- src/shared/ui/components/Button/ButtonBase.tsx
+++ src/shared/ui/components/Button/ButtonBase.tsx
@@ -1,12 +1,17 @@
 import styles from './buttonBase.module.css';
+import { JSX } from 'solid-js';
 import { useRipple } from './useRipple';
 const { root } = styles;
 
-export const ButtonBase = (props) => {
+export type ButtonBaseProps = {
+  onClick: () => void;
+} & JSX.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const ButtonBase = (props: ButtonBaseProps) => {
   const { onClick, position, tick } = useRipple();
   const handleClick = (e) => {
     onClick(e);
-    props.onClick(e);
+    props.onClick?.(e);
   };
   return <button 
     {...props}
