c53b6ac89c3ebd6a888204ceb6a12ed888916e36
ewanivannikov <ewanivannikov@gmail.com>
2024-09-29 14:15:43 +0300

    feat: requisites model

--- src/data/repositories/LevelRepository/levels.ts
+++ src/data/repositories/LevelRepository/levels.ts
@@ -10,6 +10,26 @@ export const levels = {
       'Brush.0.Up',
       'Brush.1'
     ],
+    requisites: {
+      'a': {
+        'c71a8902-9ca6-4b57-b527-63f085599c8f': {
+          pattern: [1, 1, 1],
+          hasCycle: false,
+          initialValue: 1,
+        },
+        '02263c8a-bc78-4210-834f-74967346401e': {
+          pattern: [2, 2],
+          hasCycle: false,
+          initialValue: 0,
+        },
+        '5a78b151-7bff-462d-8296-6a22f3cc7a36': {
+          pattern: [6, 1],
+          hasCycle: false,
+          initialValue: 0,
+          waiting: -1
+        }
+      }
+    }
   },
   Sierpinski: {
     slug: 'Sierpinski',
