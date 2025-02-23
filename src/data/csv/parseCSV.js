7efe4951c26d7a50334afc01d87d87a9e279cd05
Ewan Evannikov <ewanivannikov@gmail.com>
2025-02-23 20:34:20 +0300

    chore: add csv parser

---
+++ src/data/csv/parseCSV.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const results = {};
+
+fs.createReadStream('brifing.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    const versionId = row.versionId;
+    const id = row.id;
+
+    if (!results[versionId]) {
+      results[versionId] = {};
+    }
+
+    results[versionId][id] = {
+      pattern: row.pattern.split(',').map(Number),
+      hasCycle: row.hasCycle === 'TRUE',
+      initialValue: parseInt(row.initialValue, 10),
+    };
+
+    if (row.cycles !== 'NA') {
+      results[versionId][id].cycles = parseInt(row.cycles, 10);
+    }
+  })
+  .on('end', () => {
+    console.log(JSON.stringify(results, null, 2));
+  });
